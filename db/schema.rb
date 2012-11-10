14224b1474e09d9b4d320c9bcd61fc23d3dd1537
Vincent Pasquale <vincecj2000@yahoo.com>
2012-11-10 02:23:05 -0700

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121109223112) do
+ActiveRecord::Schema.define(:version => 20121110072406) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
